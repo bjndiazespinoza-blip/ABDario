0fa2c33354684145f5fd1eb938923ec673ee649c
Matías Alarcón <matias.alarconn.go@gmail.com>
2026-05-27 21:40:44 -0400

    Mejorando la función "obtenerColorAleatorio" para que siempre aparezca un color nuevo cuando se haga click sobre los headers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,28 @@
-//* Creación código script.js *//
+/* Creación código script.js */
 
-// Definiendo colores //
-const colores = ["#008000", "#0000FF", "#FF0000"];
+// Definiendo colores
+const colores = ["green", "blue", "red"];
 
-//Etiquetas H3 y h5//
+
+//Etiquetas H3 y h5
 const etiquetasH3 = document.querySelectorAll("h3, h5");
 
-//Función que retorna color aleatorio del arreglo//
+//Función que retorna color aleatorio del arreglo
+function obtenerColorAleatorio(colorActual) {
+  let indiceAleatorio = () => Math.floor(Math.random() * colores.length);
+  let nuevoColor = colores[indiceAleatorio()]
 
-function obtenerColorAleatorio() {
-  const indiceAleatorio = Math.floor(Math.random() * colores.length);
-  return colores[indiceAleatorio];
+  while (colorActual === nuevoColor) {
+      nuevoColor = colores[indiceAleatorio()];
+  }
+
+  return nuevoColor;
 }
 
 // Recorriendo cada etiqueta H3 y h5 y asignando un color aleatorio
 etiquetasH3.forEach(function(etiqueta) {
-    etiqueta.addEventListener("click", function() {
-        this.style.color = obtenerColorAleatorio();
-    });
+  etiqueta.addEventListener("click", function(event) {
+     const colorDelHeader = event.target.style.color;
+     event.target.style.color = obtenerColorAleatorio(colorDelHeader);
+  });
 });
